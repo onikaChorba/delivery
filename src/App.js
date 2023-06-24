adf9bfbbebebb52899d76524f1f2dbf922f9b7c6
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-06-24 19:59:57 +0300

    feat: fixing problem with router and load data in Api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.scss";
-// import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Shops from "./components/Shops/Shops";
 import { Header } from "../src/components/Header/Header";
 import { Provider } from "react-redux";
@@ -8,15 +7,8 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        {/* <Router> */}
-        <div>
-          <Header />
-          <Shops />
-          {/* <Routes>
-              <Route path="/" element={<Shops />} />
-            </Routes> */}
-        </div>
-        {/* </Router> */}
+        <Header />
+        <Shops />
       </div>
     </Provider>
   );
