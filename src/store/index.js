bf5372c5a8c72f24e0d91e2f454774e39ad7f4ad
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-06-24 20:48:41 +0300

    feat: connected another burger shop, changed the selection state after pressing another button, shortened the content

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import clothesSlice from "./clothesSlice";
+import burgersSlice from "./burgersSlice";
 export const store = configureStore({
   reducer: {
     clothes: clothesSlice,
+    burgers: burgersSlice,
   },
 });
