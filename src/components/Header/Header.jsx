acb302b5e7bef948d8b6766cb8bff53c3990d09b
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-07-09 20:09:17 +0300

    feat: Add registration form and handle form visibility

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Header.scss";
+import { RegistrationForm } from "../RegistrationForm/RegistrationForm";
 
 export const Header = () => {
   const [logIn, setLogIn] = useState(false);
+  const [registrForm, setRegistrForm] = useState(false);
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (formRef.current && !formRef.current.contains(event.target)) {
+        setLogIn(false);
+        setRegistrForm(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const handleLogInClick = () => {
     setLogIn(!logIn);
+    setRegistrForm(false);
+  };
+
+  const handleRegistrFormClick = () => {
+    setRegistrForm(!registrForm);
+    setLogIn(false);
   };
 
   return (
     <div className="header">
       <div className="header__logo">Delivery</div>
 
-      {logIn ? (
-        <div className="header__form">
+      <div className="header__button">
+        <button className="headerButton" onClick={handleLogInClick}>
+          Log in
+        </button>
+        <button className="headerButton" onClick={handleRegistrFormClick}>
+          Register now
+        </button>
+      </div>
+
+      {logIn && (
+        <div className="header__form" ref={formRef}>
           <form>
             <input type="text" placeholder="Username" className="input" />
             <input type="password" placeholder="Password" className="input" />
@@ -22,12 +55,11 @@ export const Header = () => {
             </button>
           </form>
         </div>
-      ) : (
-        <div className="header__button">
-          <button className="headerButton" onClick={handleLogInClick}>
-            Log in
-          </button>
-          <button className="headerButton">Register now</button>
+      )}
+
+      {registrForm && (
+        <div className="header__form" ref={formRef}>
+          <RegistrationForm />
         </div>
       )}
     </div>
