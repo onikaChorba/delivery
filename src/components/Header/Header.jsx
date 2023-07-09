88e06ce1c5fa527e6b07f2698bd05cc38ae77564
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2023-07-09 19:49:56 +0300

    feat:Update Header component with updated style structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 
 export const Header = () => {
+  const [logIn, setLogIn] = useState(false);
+
+  const handleLogInClick = () => {
+    setLogIn(!logIn);
+  };
+
   return (
     <div className="header">
       <div className="header__logo">Delivery</div>
+
+      {logIn ? (
+        <div className="header__form">
+          <form>
+            <input type="text" placeholder="Username" className="input" />
+            <input type="password" placeholder="Password" className="input" />
+            <button type="submit" className="headerButton">
+              Log in
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className="header__button">
+          <button className="headerButton" onClick={handleLogInClick}>
+            Log in
+          </button>
+          <button className="headerButton">Register now</button>
+        </div>
+      )}
     </div>
   );
 };
